56f4543759634e3c2208b8090c40ef636d33c374
Rory Cole <rory.cole1990@gmail.com>
2017-10-14 13:31:06 +0100

    Not using pytest

--- setup.py
+++ setup.py
@@ -12,5 +12,4 @@ setup(
     packages=find_packages(),
     install_requires=[line.strip() for line in open("requirements.txt")],
     long_description=open('README.md').read(),
-    tests_require=['pytest'],
 )
